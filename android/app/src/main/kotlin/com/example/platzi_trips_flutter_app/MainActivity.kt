44e97dc1f1691e317497fde5f92b869650dc1db0
AlejoRMinetti <alejorm@gmail.com>
2019-12-22 07:48:24 -0300

    Hola Mundillo - Curso Flutter de Platzi

---
+++ android/app/src/main/kotlin/com/example/platzi_trips_flutter_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.platzi_trips_flutter_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
